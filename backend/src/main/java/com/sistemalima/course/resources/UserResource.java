e52c467f1e287fde01c1639b7e06049cd1c2dee7
wagnersistemalima <wagner.sistemalima@gmail.com>
2020-10-22 23:54:41 -0300

    Service layer, component registration

--- backend/src/main/java/com/sistemalima/course/resources/UserResource.java
+++ backend/src/main/java/com/sistemalima/course/resources/UserResource.java
@@ -1,20 +1,35 @@
 package com.sistemalima.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sistemalima.course.entities.User;
+import com.sistemalima.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {                                            // controlador Rest
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {                 // 1º end point
-		User user1 = new User(1L, "Maria Silva", "maria@gmail.com", "83 93885455", "123");
-		return ResponseEntity.ok().body(user1);                      // retornar uma resposta 200 com sucesso
+	public ResponseEntity <List<User>> findAll() {                 // 1º end point buscar todos
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);                      // retornar uma resposta 200 com sucesso
+	}
+	
+	// metodo buscar por id
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);		
 	}
 
 }
